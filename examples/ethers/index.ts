1181d57fd8ce9a71f901ed9d61ab40e0554f88fc
krgko <kowpun123@gmail.com>
2021-10-19 18:23:19 +0700

    Update test for change_init_address_by_keyId

--- examples/ethers/index.ts
+++ examples/ethers/index.ts
@@ -7,9 +7,9 @@ config()
 
 const getSigner = async () => {
   try {
-    console.log('Initiating KMSProvider...')
+    console.log('1. Initiating KMSProvider...')
     const kmsProvider = new KMSProvider({
-      keyId: process.env.KEYID,
+      keyId: process.env.ANOTHER_KEYID,
       providerOrUrl: process.env.PROVIDER,
       accessKeyId: process.env.ACCESSKEYID,
       secretAccessKey: process.env.SECRETACCESSKEY,
@@ -23,7 +23,18 @@ const getSigner = async () => {
       }
     })
 
-    console.log('KMSProvider initialized')
+    console.log('2. KMSProvider initialized')
+    console.log(
+      '3. Check address before changing:',
+      await kmsProvider.getAddress()
+    )
+
+    await kmsProvider.changeInitializeAddressByKeyId(process.env.KEYID)
+    console.log(
+      '4. Check address after changing:',
+      await kmsProvider.getAddress()
+    )
+
     const signer = new ethers.providers.Web3Provider(kmsProvider).getSigner()
 
     return signer
@@ -35,27 +46,29 @@ const getSigner = async () => {
 const exampleTransaction = async () => {
   const signer = await getSigner()
   const myAddress = await signer.getAddress()
-  console.log(`KMSProvider returned public key: ${myAddress}`)
+  console.log(`5. KMSProvider returned public key: ${myAddress}`)
 
   const tokenInstance = KMSToken__factory.connect(
     process.env.TOKEN_ADDRESS,
     signer
   )
 
-  console.log('Getting tokens from faucet...')
+  console.log('6. Getting tokens from faucet...')
   const tx = await tokenInstance.faucet()
 
-  console.log('Waiting for confirmation...')
+  console.log('7. Waiting for confirmation...')
   const { status } = await tx.wait()
 
   if (!!status) {
-    console.log('Checking balance...')
+    console.log('8. Checking balance...')
     const tx = await tokenInstance.balanceOf(myAddress)
 
-    console.log(`Balance of ${myAddress} is ${tx.toString()}`)
+    console.log(`9. Balance of ${myAddress} is ${tx.toString()}`)
   }
 }
 
-exampleTransaction().then(() => {
-  process.exit(0)
-})
+exampleTransaction()
+  .then(() => {
+    process.exit(0)
+  })
+  .catch((e) => console.log(e))
